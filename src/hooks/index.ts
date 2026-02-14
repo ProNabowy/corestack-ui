676bb733e74c3452b4da6636ee503dcadfe6df3d
ProNabowy <96296770+ProNabowy@users.noreply.github.com>
2026-02-14 10:41:47 +0200

    feat: add Prettier and organize imports plugin; update project structure with ThemeProvider and clean up App component

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,2 +1,1 @@
-// Export hooks for library consumers
-export * from '../components/Autocomplete/hooks/useAutocomplete';
+export {};
--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,4 @@ export * from "./hooks";
 export * from "./utils";
 export * from "./types";
 export * from "./styles";
+export * from "./providers";
